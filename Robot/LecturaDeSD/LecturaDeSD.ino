7f86e6df965603fd6bd5335f3a6a97e2d1ddfe24
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2021-08-25 18:47:07 -0300

    cambios previos al pull

--- Robot/LecturaDeSD/LecturaDeSD.ino
+++ Robot/LecturaDeSD/LecturaDeSD.ino
@@ -1,3 +1,4 @@
+#include <Ticker.h>
 
 #include <Arduino.h>
 #include "FS.h"
@@ -12,10 +13,14 @@
 
 String datosW[2];
 
+Ticker ledRojoOK( parpadeoLed(LED_PIN_R), 500 );
+
+//Función para controlar el parpadeo de los LEDs
 void parpadeoLed(int pinLed){
   digitalWrite(pinLed, !digitalRead(pinLed));
 }
 
+//Función para escanear las redes WiFi cercanas
 bool escanearWifi(){
   Serial.println("Escaneando redes WiFi...");
     bool res = false;
@@ -51,7 +56,7 @@ bool escanearWifi(){
 
 
 
-void conectarWifi(const char* ssid, const char* pass){
+bool conectarWifi(const char* ssid, const char* pass){
     Serial.print("Connecting to ");
     Serial.println(ssid);
 
@@ -59,7 +64,7 @@ void conectarWifi(const char* ssid, const char* pass){
 
     while (WiFi.status() != WL_CONNECTED) {
         delay(250);
-        parpadeoLed(LED_PIN_R);
+        parpadeoLed(LED_PIN_B);
     }
 
     Serial.println("");
@@ -96,13 +101,14 @@ bool leerArchivoWiFi(fs::FS &fs, const char * path){
     return true;
 }
 
+
+//Función para inicializar al módulo lector de tarjetas SD
 bool inicializarSd(){
   if(!SD.begin()){
         Serial.println("Error: No se pudo montar tarjeta SD");
         return false;
     }
     
-    //
     uint8_t cardType = SD.cardType();
 
     if(cardType == CARD_NONE){
@@ -110,76 +116,64 @@ bool inicializarSd(){
         return false;
     }
 
-    Serial.print("Tipo de tarjeta SD: ");
-    if(cardType == CARD_MMC){
-        Serial.println("MMC");
-    } else if(cardType == CARD_SD){
-        Serial.println("SDSC");
-    } else if(cardType == CARD_SDHC){
-        Serial.println("SDHC");
-    } else {
-        Serial.println("UNKNOWN");
-    }
-
     uint64_t cardSize = SD.cardSize() / (1024 * 1024);
     Serial.printf("Tamaño de tarjeta SD: %lluMB\n", cardSize);
     return true;
 }
 
-void 
-
-
+void inicializarWifi(){
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(2000);
+}
 
 void setup() {
   // put your setup code here, to run once:
-      pinMode(LED_PIN_R, OUTPUT);
-      pinMode(LED_PIN_G, OUTPUT);
-      pinMode(LED_PIN_B, OUTPUT);
+  pinMode(LED_PIN_R, OUTPUT);
+  pinMode(LED_PIN_G, OUTPUT);
+  pinMode(LED_PIN_B, OUTPUT);
       
-      Serial.begin(115200);
-      Serial.println("Inicializando");
-      digitalWrite(LED_PIN_R, HIGH);
-    // Set WiFi to station mode and disconnect from an AP if it was previously connected
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(2000);
+  Serial.begin(115200);
+  Serial.println("Inicializando");
+ 
+  
+    // WiFi - Fase 1: Pasar a modo cliente y prepararse para la conexión a la red
+    //  de la configuración
+    digitalWrite(LED_PIN_R, HIGH);
+    inicializarWifi();
+    
+    //Variables para los datos de la configuración de red
+    const char* ssid;
+    const char* pass;
 
     //Inicializando SD
     digitalWrite(LED_PIN_G, HIGH);
-    const char* ssid;
-    const char* pass;
     
     if( !inicializarSd() ){
-        if( leerArchivoWiFi(SD,"/wifi.txt" )){
-          ssid = datosW[0].c_str();
-          pass = datosW[1].c_str();
-        }
-        else{
-          
+      while(true){
+          digitalWrite(LED_PIN_G, !digitalRead(LED_PIN_R));
+          delay(250);
         }
     }
     else{
-      while(true){
-          digitalWrite(LED_PIN_G, !digitalRead(LED_PIN_R));
-          delay(500);
+        if( leerArchivoWiFi(SD,"/wifi.txt" )){
+          ssid = datosW[0].c_str();
+          pass = datosW[1].c_str();
+          digitalWrite(LED_PIN_G, HIGH);
         }
     }
-    
-    
-    
-    
-    
-    
-    //pass = datosW[1];
-    
-    escanearWifi();
 
-    conectarWifi(ssid, pass);
     
-    while(true){
-      digitalWrite(LED_PIN_R, !digitalRead(LED_PIN_R));
-      delay(2000);
-      digitalWrite(LED_PIN_G, !digitalRead(LED_PIN_G));
+    //WiFi - Fase 2 - Conexión a la red obtenida de la configuración
+    digitalWrite(LED_PIN_B, HIGH);
+    if( escanearWifi() ){
+      conectarWifi(ssid, pass);
+    }
+    else{
+        while(true){
+          digitalWrite(LED_PIN_B, !digitalRead(LED_PIN_B));
+          delay(250);
+        }
     }
 
 }
